c6f4703478ff6f86ba638cad72b2be8b282334f0
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-06 18:30:21 -0300

    final?

---
+++ ejemplo-barberia-turnos-mvc/barberia-turnos-mvc/Migrations/20260806201540_migracionesRestantes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace barberia_turnos_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracionesRestantes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Clientes_BarberiaId_Telefono",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Clientes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_BarberiaId_Telefono",
+                table: "Clientes",
+                columns: new[] { "BarberiaId", "Telefono" },
+                unique: true,
+                filter: "[Telefono] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Clientes_BarberiaId_Telefono",
+                table: "Clientes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Clientes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_BarberiaId_Telefono",
+                table: "Clientes",
+                columns: new[] { "BarberiaId", "Telefono" },
+                unique: true);
+        }
+    }
+}
